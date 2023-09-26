32b89da23de016c2b1910d663acda1d6e536d410
Allan Medeiros <medeiros.allan@gmail.com>
2023-09-26 12:02:02 -0300

    Aula 22 - Blade directives: continuação

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -12,24 +12,21 @@
         <br>
     </H1>
 
-    @if (count($tasks))
-        @foreach ($tasks as $task)
-            <div class="card border-dark mb-3" style="max-width: 18rem;">
-                <h4 class="card-header"><b>{{ $task->title }}</b></h4>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $task->description }}</h5>
-                    <p class="card-text">{{ $task->long_description }}</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-body-secondary">Updated at {{ $task->updated_at }} </small>
-                </div>
+    @forelse ($tasks as $task)
+        <div class="card border-dark mb-3" style="max-width: 18rem;">
+            <h4 class="card-header"><b>{{ $task->title }}</b></h4>
+            <div class="card-body">
+                <h5 class="card-title">{{ $task->description }}</h5>
+                <p class="card-text">{{ $task->long_description }}</p>
             </div>
-            <br>
-        @endforeach
-    @else
+            <div class="card-footer">
+                <small class="text-body-secondary">Updated at {{ $task->updated_at }} </small>
+            </div>
+        </div>
+        <br>
+    @empty
         <div>There are no tasks</div>
-    @endif
-
+    @endforelse
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
