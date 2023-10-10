99037e41020f406ac0d222c5bd3690e15c350020
Allan Medeiros <medeiros.allan@gmail.com>
2023-10-10 10:13:10 -0300

    24 - Running a MySQL Database and Client Using Docker

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
     <ul class="list-group">
-      @forelse ($tasks as $task)
-        <li class="list-group-item"><a href="{{ route('tasks.show', ['id'=> $task->id]) }}"><i class="fa-brands fa-odysee"></i></a> {{ $task->title }}</li>
-      @empty
-        <div>There are no tasks</div>
-      @endforelse
+        @forelse ($tasks as $task)
+            <li class="list-group-item"><a href="{{ route('tasks.show', ['id'=> $task->id]) }}"><i class="fa-brands fa-odysee"></i></a> {{ $task->title }}</li>
+        @empty
+            <div>There are no tasks</div>
+        @endforelse
     </ul>
 @endsection
