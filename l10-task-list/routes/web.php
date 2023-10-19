4b7ad6654f1781cf15b8c631c37786bc981ad8a7
Allan Medeiros <medeiros.allan@gmail.com>
2023-10-19 07:04:27 -0300

    Aula 30 - Validating and Storing Data

--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -15,27 +16,13 @@ use Illuminate\Http\Request;
 |
 */
 
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
 Route::get('/', function(){
     return redirect()->route('tasks.index');
 });
 
 Route::get('/tasks', function () {
     return view('index',[
-        'tasks'=>\App\Models\Task::latest()->get(),
+        'tasks'=>Task::latest()->get(),
     ]);
 })->name('tasks.index');
 
@@ -43,11 +30,24 @@ Route::view('/tasks/create','create')
     ->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
-  return view('show', ['task'=>\App\Models\Task::findOrFail($id)]);
+  return view('show', ['task'=>Task::findOrFail($id)]);
 })->name('tasks.show');
 
 Route::post('/tasks', function(Request $request){
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = new Task();
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 // Route::get('/hello', function () {
