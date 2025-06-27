fa9d435438c63bd3e0438b40bb58f49f3dbd3786
Allan Medeiros <medeiros@codata.pb.gov.br>
2025-06-27 14:31:39 -0300

    Aula 44 - feat: add fillable fields to Review model and query scopes to Book model

--- book-review/app/Models/Review.php
+++ book-review/app/Models/Review.php
@@ -9,6 +9,10 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'review',
+        'rating',
+    ];
     public function book()
     {
         return $this->belongsTo(Book::class);
